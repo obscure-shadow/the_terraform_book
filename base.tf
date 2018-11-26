be3581da0ddafff6a96fef473305ad1310e4c347
David Paul <dfp333@gmail.com>
2018-11-26 15:41:21 -0600

    added eip resource

--- base.tf
+++ base.tf
@@ -2,9 +2,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
-#so far it appears that the word in the second set of " " is the name you want to call it,
-#does the first one have to be exactly what you are creating or can this name also be anything?
+# so far it appears that the word in the second set of " " is the name you want to call it,
+# does the first one have to be exactly what you are creating or can this name also be anything?
+# looks like the first set of " " specifies the provider and then type
+# so probably specific to the provider you are using.
 resource "aws_instance" "base" {
   ami           = "ami-0d729a60"
   instance_type = "t2.micro"
+}
+
+resource "aws_eip" "base" {
+  instance = "${aws_instance.base.id}"
 }
